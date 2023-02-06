b69987521b536b912d50431c7c3fdb63401f0f44
shahawlhs <shaha@wlhs.wlwv.k12.or.us>
2023-02-05 21:04:14 -0800

    Polygons

---
+++ src/Polygon.java
@@ -0,0 +1,93 @@
+import java.text.DecimalFormat;
+
+/**
+ * @file Polygon Java
+ * @date 2/2/23
+ * @author Ajay Shah
+ *
+ * @description Creates a polygon with a user determined number of sides, side length, ans shape type.
+ */
+
+public class Polygon {
+    //instance variables
+    private int sides;
+    private double length;
+    private String type;
+
+    //default constructor
+    public Polygon(){
+        sides = 3;
+        length = 1.0;
+        type = "Triangle";
+    }
+
+
+    //overload constructor
+
+    /**
+     * Creates a Polygon object with sides, length, and type
+     *
+     * @param s number of sides the polygon has
+     * @param l length of each side
+     * @param t type of polygon
+     */
+
+    public Polygon(int s, double l, String t){
+        if (s > 2){
+            sides = s;
+        }
+        else {
+            sides = 3;
+        }
+        if (l != 0.0){
+            length = l;
+        }
+        else {
+            length = 1.0;
+        }
+
+        type = t;
+    }
+
+
+    //accessors
+
+    /**
+     *
+     * @return the number of sides on the polygon
+     */
+
+    public int getNumSides(){
+        return sides;
+    }
+
+    /**
+     *
+     * @return the length of each side of the polygon
+     */
+
+    public double getSideLength(){
+        return length;
+    }
+
+    /**
+     *
+     * @return the type of polygon
+     */
+
+    public String getShapeType(){
+        return type;
+    }
+
+    //toString method
+
+    /**
+     *
+     * @return Prints the type of polygon with the number of sides and the length of each side
+     */
+
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("#.##");
+        return "Your shape is a " + type + " with " + sides + " sides and a length of " + df.format(length) + ".";
+    }
+}
---
+++ src/PolygonTester.java
@@ -0,0 +1,11 @@
+public class PolygonTester {
+    public static void main(String [] args) {
+        Polygon poly1 = new Polygon(5, 6.865, "pentagon");
+        System.out.println(poly1);
+
+        Polygon poly2 = new Polygon(1, 2.73, "Line");
+        System.out.println(poly2);
+
+
+    }
+}
